72aeadfb77f9b96ec5a5716285978789a0d228df
vu tri tam <vutritamiuem@gmail.com>
2022-05-24 01:36:23 +0700

    fix cors axios

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -6,7 +6,10 @@ import axios from "axios"
 
 const instance = axios.create({
     baseURL: 'https://manager-app-sever.herokuapp.com',
-    headers: { "Access-Control-Allow-Origin": "*" }
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Content-Type': 'application/json'
+    }
 
 });
 
